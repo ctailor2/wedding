795cbe3f69df674d5e17d54268c041ab476cad17
Chirag Tailor <ctailor2@gmail.com>
2014-03-13 23:13:42 -0500

    Flash partial and helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	def flash_class(type)
+		type_class = case type
+		when :success then "alert-success"
+		when :info then "alert-info"
+		when :warning then "alert-warning"
+		when :danger then "alert-danger"
+		end
+
+		"alert #{type_class} alert-dismissable"
+	end
 end
