875a8db24bb5b12690afd536a6e3fc3d95b2abab
Chirag Tailor <ctailor2@gmail.com>
2014-03-22 13:20:14 -0500

    Prepare for heroku seeding. Uma invites seeding.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,28 @@
-group = Group.create(rsvp_code: "1234", name: "Test Group")
-AgeGroup.create(description: "Adult (12+)")
-AgeGroup.create(description: "Child Under 12")
-AgeGroup.create(description: "Child Under 5")
-vidhi = Event.create(title: "Vidhi")
-manglik_prasango = Event.create(title: "Manglik Prasango")
-garba = Event.create(title: "Raas-Garba")
-wedding = Event.create(title: "Wedding")
-reception = Event.create(title: "Reception")
-manglik_prasango.invites.create(group_id: group.id, num_invited: 1)
-garba.invites.create(group_id: group.id, num_invited: 2)
-wedding.invites.create(group_id: group.id, num_invited: 3)
-reception.invites.create(group_id: group.id, num_invited: 4)
+AgeGroup.find_or_create_by_description(description: "Adult (12+)")
+AgeGroup.find_or_create_by_description(description: "Child Under 12")
+AgeGroup.find_or_create_by_description(description: "Child Under 5")
+Event.find_or_create_by_title(title: "Vidhi")
+Event.find_or_create_by_title(title: "Manglik Prasango")
+Event.find_or_create_by_title(title: "Raas-Garba")
+Event.find_or_create_by_title(title: "Wedding")
+Event.find_or_create_by_title(title: "Reception")
+
+uma_data = GoogleSpreadsheet.new("0Aht20VD6GO7odG5OOXVJbXlpbmtNS01Kek4wbjc3Smc")
+uma_data.group_invite_data.each_with_index do |data_row, index|
+	name = data_row[0]
+	invite_data = data_row[1..-2]
+	group = Group.create(name: name)
+	uma_data.worksheet[index + 2, 6] = group.rsvp_code
+
+	event_invite_data = invite_data.zip(uma_data.event_ids)
+	event_invite_data.each do |data|
+		num_invited = data.first.to_i
+		event_id = data.last
+
+		unless num_invited.blank? || num_invited == 0
+			group.invites.create(num_invited: num_invited, event_id: event_id)
+		end
+	end
+end
+
+uma_data.worksheet.save()
