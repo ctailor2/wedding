300d46684b80396783e73007694b96f13fbf79e6
Chirag Tailor <ctailor2@gmail.com>
2014-04-12 20:33:07 -0500

    Update seeds file for importing chirag's invite data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@ Event.find_or_create_by_title(title: "Raas-Garba")
 Event.find_or_create_by_title(title: "Wedding")
 Event.find_or_create_by_title(title: "Reception")
 
+chirag_worksheet = GoogleWorksheet.new("0Aht20VD6GO7odGlJQm1hS0tsd3NLbkd1dUQ5Tk1OdEE")
+chirag_worksheet.import
+chirag_worksheet.save
+
